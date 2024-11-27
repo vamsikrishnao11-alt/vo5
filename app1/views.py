01d86f0d59baf8e0b65766a2d8fae010d2ca1fcf
kayamvamsikrishna <kayamvamsikrishna@gmail.com>
2024-11-27 13:39:55 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def vamsi(request):
+    return render(request,'h1.html')
+def poojitha(request):
+    return render(request,'h2.html')
